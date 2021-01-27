9f49e2ca86f5c9239583fc1d5d7cb9b23465b132
CREESTL <kroymw3@yandex.ru>
2021-01-27 23:22:07 +0300

    some minor changes to first version of modules drawing

--- gnomes/main.py
+++ gnomes/main.py
@@ -35,50 +35,40 @@ num_modules = 8
 # a list of all modules of a cube
 modules = []
 
-# def create_images():
-#     images = []
-#     for i in range(num_screens):
-#         img = np.zeros((240, 240, 3), np.uint8)
-#         cv2.rectangle(img, (0, 0), (240, 240), (255, 255, 0), -1)
-#         cv2.putText(img, str(i), (120, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-#         images.append(img)
-#     return images
 
-x, y = 0, 120
+# with each request module screens are updated
 def update_screens():
-    global x, y
-    module_0 = Module(0)
-    module_1 = Module(1)
+    # using all 8 modules of the cube
+    modules = [Module(i) for i in range(8)]
     screens = []
-    # 3 used screens of module (object could be just on one of them)
-    filled_screens_0 = module_0.draw_point(120, 360)
-    filled_screens_1 = module_1.draw_point(294, 120)
-    screens = filled_screens_0 + filled_screens_1
+    for module in modules:
+        for screen in module.draw_point(70, 70):
+            screens.append(screen)
+    # just to double-check that all 24 screens are present
     while len(screens) != num_screens:
+        # filling up the rest of empty screens
         screens.append(np.zeros((240, 240, 3), np.uint8))
     return screens
 
 
-
-# draws plain green modules correctly
 @app.route('/test', methods=['GET', 'POST'])
 def draw():
     modules = []
     img_num = 0
     # create a list of output images (8 modules * 3 images on each)
     images = update_screens()
-
     # put the images into the response archive
     memory_file = io.BytesIO()
     for i in range(8):
         modules.append(request.json['modules'][i])
-    # for i, m in enumerate(modules):
-    #     print(f'MODULE {i}')
-    #     for j, s in enumerate(m['screens']):
-    #         print(f'--screen {j}')
-    #         print(f'----counter clockwise: module {s["top"][0]} screen {s["top"][1]}')
-    #         print(f'----clockwise: module {s["left"][0]} screen {s["left"][1]}')
-    # print("==========\n")
+    # information about relative position of screens and modules
+    for i, m in enumerate(modules):
+        print(f'MODULE {i}')
+        for j, s in enumerate(m['screens']):
+            print(f'--screen {j}')
+            print(f'----counter clockwise: module {s["top"][0]} screen {s["top"][1]}')
+            print(f'----clockwise: module {s["left"][0]} screen {s["left"][1]}')
+    print("==========\n")
     with ZipFile(memory_file, "w") as zip_file:
         for module in range(num_modules):
             for screen in range(num_screens // num_modules):
@@ -98,8 +88,6 @@ def draw():
     response = make_response(memory_file.read())
     response.headers['Content-Type'] = 'application/zip'
     return response
-
-
 
 
 if __name__ == "__main__":
--- gnomes/modules_drawing.py
+++ gnomes/modules_drawing.py
@@ -8,49 +8,52 @@ Here should be functions for transforming object coordinates to sphero-cubic coo
 import numpy as np
 import cv2
 
+
+# class represents a single screen
 class Screen:
     def __init__(self):
+        # all objects are projected on its surface
         self.surface = np.zeros((240, 240, 3), np.uint8)
-        self.size = 240
 
     # function gets a clockwise and anticlockwise neighbours of a screen
     def update_neighbours(self, modules):
         return
 
+# class represents a single module
 class Module:
     def __init__(self, num):
+        # a number of module (max 8)
         self.num = num
+        # three screens of a module
         self.screens = [Screen().surface for i in range(3)]
-
 
     # function draws a circle using module coordinates system
     def draw_point(self, x, y):
+        color = (255, 255, 0)
+        r = 10
+        thikness = 8
         # zero screen
         # origin at lower right corner
         # y - upwards, x - to the left
         if (x < 240) and (y < 240):
             screen = self.screens[0]
-            screen_width = screen.shape[0]
-            screen_height = screen.shape[1]
-            cv2.circle(screen, (y, x), 5, (255, 255, 0), 5)
+            cv2.circle(screen, (y, x), r, (255, 255, 0), thikness)
             return self.screens
         # first screen
         # origin at top right corner
         # y - to the left, x - to us
         elif (x >= 240) and (y < 240):
             screen = self.screens[1]
-            screen_width = screen.shape[0]
             screen_height = screen.shape[1]
-            cv2.circle(screen, (screen_height - (x - 240), y), 5, (255, 255, 0), 5)
+            cv2.circle(screen, (screen_height - (x - 240), y), r, (255, 255, 0), thikness)
             return self.screens
         # second screen
         # origin at lower left corner
         # x - upwards, y - to the right
         elif (x < 240) and (y >= 240):
             screen = self.screens[2]
-            screen_width = screen.shape[0]
             screen_height = screen.shape[1]
-            cv2.circle(screen, (x, screen_height - (y - 240)), 5, (255, 255, 0), 5)
+            cv2.circle(screen, (x, screen_height - (y - 240)), r, (255, 255, 0), thikness)
             return self.screens
 
 
